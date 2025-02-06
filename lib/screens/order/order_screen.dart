bfd738d4439dc08c3429f4e23c73cb310e05bcf2
phy122 <qkrghdud369@gmail.com>
2025-02-06 18:30:52 +0900

    주문 내역 + 상세 페이지 수정

--- lib/screens/order/order_screen.dart
+++ lib/screens/order/order_screen.dart
@@ -8,8 +8,73 @@ class OrderScreen extends StatefulWidget {
 }
 
 class _OrderScreenState extends State<OrderScreen> {
+  final List<String> items =
+      List<String>.generate(100, (index) => "Item $index");
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return DefaultTabController(
+      length: 2, // 탭의 수
+      child: Scaffold(
+        appBar: AppBar(
+          leading: GestureDetector(
+            onTap: () {
+              Navigator.pushReplacementNamed(context, "/menu/list");
+            },
+            child: Icon(Icons.arrow_back),
+          ),
+          title: Text("주문 내역"),
+          bottom: TabBar(
+            labelColor: Color.fromARGB(255, 255, 136, 0),
+            indicatorColor: Color.fromARGB(255, 255, 136, 0),
+            tabs: [
+              Tab(text: "장바구니"),
+              Tab(text: "주문내역"),
+            ],
+          ),
+        ),
+        body: TabBarView(
+          children: [
+            Center(child: Text("장바구니 내역")),
+            Column(
+              children: [
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: items.length,
+                    itemBuilder: (context, index) {
+                      return Column(
+                        children: [
+                          ListTile(
+                            title: Text(items[index]),
+                            subtitle: Text('Subtitle $index'),
+                            selected: index < 3,
+                            contentPadding: EdgeInsets.symmetric(
+                                vertical: 20.0, horizontal: 16.0),
+                            onTap: () {
+                              Navigator.pushNamed(
+                                context,
+                                '/order/detail',
+                                arguments: {
+                                  'item': items[index],
+                                  'index': index
+                                },
+                              );
+                            },
+                          ),
+                          Container(
+                            height: 1,
+                            color: Colors.black,
+                          ),
+                        ],
+                      );
+                    },
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
