4d659b4a25240ba5658a6e804dcb88e92f8276cc
koko2807 <0705ldh@hanmail.net>
2025-02-11 18:21:47 +0900

    장바구니 모달

--- lib/screens/cart/cart_screen.dart
+++ lib/screens/cart/cart_screen.dart
@@ -8,8 +8,10 @@ class CartScreen extends StatefulWidget {
 }
 
 class _CartScreenState extends State<CartScreen> {
+  bool isChecked = false;
   void _showMenu() {
     showModalBottomSheet(
+      backgroundColor: Colors.white,
       context: context,
       shape: const RoundedRectangleBorder(
         borderRadius: BorderRadius.vertical(top: Radius.circular(20)),
@@ -23,23 +25,90 @@ class _CartScreenState extends State<CartScreen> {
               ListTile(
                 leading: const Icon(Icons.arrow_back_ios_new_rounded),
                 title: const Text(
-                  "불고기 버거",
+                  "김치찌개",
                   style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
                 ),
                 onTap: () => Navigator.pop(context),
               ),
+              const Divider(),
               ListTile(
                 leading: const Icon(Icons.list),
                 title: const Text("옵션변경", style: TextStyle(fontSize: 20)),
               ),
-              ListTile(
-                title: const Text("", style: TextStyle(fontSize: 20)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: isChecked,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            isChecked = value ?? false;
+                          });
+                        },
+                      ),
+                      const Text(
+                        "공기밥",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  const Text(
+                    "+ 1,000원",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
               ),
-              ListTile(
-                title: const Text("", style: TextStyle(fontSize: 20)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: isChecked,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            isChecked = value ?? false;
+                          });
+                        },
+                      ),
+                      const Text(
+                        "라면 사리",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  const Text(
+                    "+ 1,000원",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
               ),
-              ListTile(
-                title: const Text("", style: TextStyle(fontSize: 20)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Row(
+                    children: [
+                      Checkbox(
+                        value: isChecked,
+                        onChanged: (bool? value) {
+                          setState(() {
+                            isChecked = value ?? false;
+                          });
+                        },
+                      ),
+                      const Text(
+                        "당면 사리",
+                        style: TextStyle(fontSize: 18),
+                      ),
+                    ],
+                  ),
+                  const Text(
+                    "+ 1,000원",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ],
               ),
               const SizedBox(height: 16),
               Container(
@@ -70,145 +139,156 @@ class _CartScreenState extends State<CartScreen> {
     );
   }
 
-  void _deleteItem() {
-    // print("항목이 삭제되었습니다.");
-  }
+  void _deleteItem() {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
-        leading: const Icon(Icons.arrow_back_ios_new_rounded),
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          icon: const Icon(Icons.arrow_back_ios_new_rounded),
+          onPressed: () {
+            Navigator.pop(context);
+          },
+        ),
         centerTitle: false,
         title: const Text("장바구니", style: TextStyle(fontWeight: FontWeight.bold)),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            const Text(
-              "메인메뉴",
-              style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
-            ),
-            const Divider(),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text(
+                "메인메뉴",
+                style: TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+              ),
+              const Divider(),
 
-            // 첫 번째 메뉴
-            ListTile(
-              contentPadding: EdgeInsets.zero,
-              leading: ClipRRect(
-                borderRadius: BorderRadius.circular(10),
-                child: Image.asset(
-                  'assets/images/menu1.jpg',
-                  width: 60,
-                  height: 60,
-                  fit: BoxFit.cover,
+              // 첫 번째 메뉴
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    'assets/images/menu1.jpg',
+                    width: 60,
+                    height: 60,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                title: const Text("김치찌개"),
+                subtitle: const Text("가격: 8,000원 | 수량: 1개"),
+                // 옵션변경 버튼
+                trailing: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(
+                      style: (ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 197, 197, 197), minimumSize: const Size(100, 30))),
+                      onPressed: _showMenu,
+                      child: const Text(
+                        "옵션변경",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                    // SizedBox(width: 50),
+                    // ElevatedButton(
+                    //   style: ElevatedButton.styleFrom(
+                    //     backgroundColor: Colors.redAccent,
+                    //     minimumSize: const Size(100, 30),
+                    //   ),
+                      //   onPressed: _deleteItem,
+                    //   child: const Text(
+                    //     "삭제하기",
+                    //     style: TextStyle(
+                    //       fontWeight: FontWeight.bold,
+                    //       color: Colors.white,
+                    //     ),
+                    //   ),
+                    // ),
+                  ],
                 ),
               ),
-              title: const Text("김치찌개"),
-              subtitle: const Text("가격: 8,000원 | 수량: 1개"),
-              // 옵션변경 버튼
-              trailing: Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  ElevatedButton(
-                    style: (ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 197, 197, 197),
-                    )),
-                    onPressed: _showMenu,
-                    child: const Text(
-                      "옵션변경",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.black,
+              const Divider(),
+
+              // 두 번째 메뉴
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    'assets/images/menu2.jpg',
+                    width: 60,
+                    height: 60,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                title: const Text("떡볶이"),
+                subtitle: const Text("가격: 7,000원 | 수량: 1개"),
+                // 옵션변경 버튼
+                trailing: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(
+                      style: (ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 197, 197, 197), minimumSize: const Size(100, 30))),
+                      onPressed: _showMenu,
+                      child: const Text(
+                        "옵션변경",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
                       ),
                     ),
-                  ),
-                  SizedBox(height: 5),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: Colors.redAccent,
-                    ),
-                    onPressed: _deleteItem,
-                    child: const Text(
-                      "삭제하기",
-                      style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        color: Colors.white,
-                      ),
-                    ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-            // const Divider(),
+              const Divider(),
 
-            // // 두 번째 메뉴
-            // ListTile(
-            //   contentPadding: EdgeInsets.zero,
-            //   leading: ClipRRect(
-            //     borderRadius: BorderRadius.circular(10),
-            //     child: Image.asset(
-            //       'assets/images/menu2.jpg',
-            //       width: 60,
-            //       height: 60,
-            //       fit: BoxFit.cover,
-            //     ),
-            //   ),
-            //   title: const Text("떡볶이"),
-            //   subtitle: const Text("가격: 7,000원 | 수량: 1개"),
-            //   // 옵션변경 버튼
-            //   trailing: SizedBox(
-            //     width: 90,
-            //     height: 35,
-            //     child: FloatingActionButton(
-            //       backgroundColor: const Color.fromARGB(255, 197, 197, 197),
-            //       onPressed: _showMenu,
-            //       child: const Text(
-            //         "옵션변경",
-            //         style: TextStyle(
-            //           fontWeight: FontWeight.bold,
-            //           color: Colors.black,
-            //         ),
-            //       ),
-            //     ),
-            //   ),
-            // ),
-            // const Divider(),
-
-            // // 세 번째 메뉴
-            // ListTile(
-            //   contentPadding: EdgeInsets.zero,
-            //   leading: ClipRRect(
-            //     borderRadius: BorderRadius.circular(10),
-            //     child: Image.asset(
-            //       'assets/images/menu3.jpg',
-            //       width: 60,
-            //       height: 60,
-            //       fit: BoxFit.cover,
-            //     ),
-            //   ),
-            //   title: const Text("네란버거"),
-            //   subtitle: const Text("가격: 5,500원 | 수량: 1개"),
-            //   // 옵션변경 버튼
-            //   trailing: SizedBox(
-            //     width: 90,
-            //     height: 35,
-            //     child: FloatingActionButton(
-            //       backgroundColor: const Color.fromARGB(255, 197, 197, 197),
-            //       onPressed: _showMenu,
-            //       child: const Text(
-            //         "옵션변경",
-            //         style: TextStyle(
-            //           fontWeight: FontWeight.bold,
-            //           color: Colors.black,
-            //         ),
-            //       ),
-            //     ),
-            //   ),
-            // ),
-            // const Divider(),
-          ],
+              // 세 번째 메뉴
+              ListTile(
+                contentPadding: EdgeInsets.zero,
+                leading: ClipRRect(
+                  borderRadius: BorderRadius.circular(10),
+                  child: Image.asset(
+                    'assets/images/menu3.jpg',
+                    width: 60,
+                    height: 60,
+                    fit: BoxFit.cover,
+                  ),
+                ),
+                title: const Text("네란버거"),
+                subtitle: const Text("가격: 5,500원 | 수량: 1개"),
+                // 옵션변경 버튼
+                trailing: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    ElevatedButton(
+                      style: (ElevatedButton.styleFrom(
+                          backgroundColor: const Color.fromARGB(255, 197, 197, 197), minimumSize: const Size(100, 30))),
+                      onPressed: _showMenu,
+                      child: const Text(
+                        "옵션변경",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              const Divider(),
+            ],
+          ),
         ),
       ),
     );
