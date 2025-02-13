2a298781bb6d8cf8d66adceaedece9d96e227f34
Yoo-JiHyun <luhyun26@gmail.com>
2025-02-13 13:13:41 +0900

    수정

--- lib/main.dart
+++ lib/main.dart
@@ -35,7 +35,7 @@ class MyApp extends StatelessWidget {
           scaffoldBackgroundColor: Colors.white,
           useMaterial3: true,
           primaryColor: Color.fromRGBO(255, 102, 0, 1)),
-      initialRoute: '/payment/pay',
+      initialRoute: '/main',
       routes: {
         '/main': (context) => const MainScreen(),
         '/menu/list': (context) => const MenuScreen(),
